08f17825c81f8b758ee562fabfed018fca05c4ab
srsebbi <csperichon@gmail.com>
2023-04-18 19:55:13 -0300

    Created User Controller and added GET user and POST User

---
+++ MockClothesCommerceAPI/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MockClothesCommerceAPI.Contracts.User;
+
+namespace MockClothesCommerceAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult CreateUser(CreateUserRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetUser(int id)
+    {
+        return Ok(id);
+    }
+
+}
+
