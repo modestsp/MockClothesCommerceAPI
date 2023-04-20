b71692219a3fce54a273268c9a65337bddfa9245
srsebbi <csperichon@gmail.com>
2023-04-19 21:05:38 -0300

    Create ServiceErrors for global error handling, and added Errors.User for errors related to users endpont

---
+++ MockClothesCommerceAPI/ServiceErrors/Errors.User.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+
+namespace MockClothesCommerceAPI.ServiceErrors;
+
+public class Errors
+{
+    public static class User
+    {
+        public static Error NotFound => Error.NotFound(
+            code: "User.NotFound",
+            description: "User was not found");
+    }
+}
