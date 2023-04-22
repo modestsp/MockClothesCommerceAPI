3a962c2bcbbfc10711d87e528349722c2c42e9e5
srsebbi <csperichon@gmail.com>
2023-04-21 21:18:30 -0300

    Create record for Update, Create and Get products

---
+++ MockClothesCommerceAPI.Contracts/Product/CreateProductRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MockClothesCommerceAPI.Contracts.Product;
+
+public record CreateProductRequest(
+    string Name,
+    string Description,
+    decimal Price,
+    //  int CategoryId,
+    List<string> Tags);
+
---
+++ MockClothesCommerceAPI.Contracts/Product/UpdateProductRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MockClothesCommerceAPI.Contracts.Product;
+
+public record UpdateProductRequest(
+    string Name,
+    string Description,
+    decimal Price,
+    //  int CategoryId,
+    List<string> Tags);
+
