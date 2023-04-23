111f8a69a1950d149b0e32832febfff4e976580b
srsebbi <csperichon@gmail.com>
2023-04-23 20:39:40 -0300

    Added AutoMapper profiles for category, user, product, review

---
+++ MockClothesCommerceAPI/Helper/MappingProfiles.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MockClothesCommerceAPI.Dtos;
+using MockClothesCommerceAPI.Models;
+
+namespace MockClothesCommerceAPI.Helper;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        // Category
+        CreateMap<Category, GetCategoryResponse>();
+
+        // Product
+        CreateMap<CreateProductRequest, Product>();
+        CreateMap<Product, CreateProductResponse>();
+        CreateMap<Product, GetProductResponse>();
+
+        // User
+        CreateMap<CreateUserRequest, User>();
+
+        // Review
+        CreateMap<CreateReviewRequest, Review>();
+        CreateMap<Review, CreateReviewResponse>();
+        CreateMap<Review, GetReviewResponse>()
+            .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.User.Username))
+            .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.Name))
+            .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.User.Id))
+            .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.Product.Id));
+
+    }
+}
+
