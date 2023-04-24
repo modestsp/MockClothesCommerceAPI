9129decbf3a653f47a4be5f3f6a331a2e408d1ff
srsebbi <csperichon@gmail.com>
2023-04-24 20:32:54 -0300

    Added DTO for favorite product

---
+++ MockClothesCommerceAPI/Dtos/FavoriteProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MockClothesCommerceAPI.Dtos;
+public class CreateFavoriteRequest
+{
+    public int UserId { get; set; }
+    public int ProductId { get; set; }
+}
+
+public class DeleteFavoriteRequest
+{
+    public int UserId { get; set; }
+    public int ProductId { get; set; }
+}
