dd669a151a98c5c1d7692318d0efef69e56ad6bf
srsebbi <csperichon@gmail.com>
2023-04-25 21:21:41 -0300

    Added services for create a review for a product and create, delete favorite product

--- MockClothesCommerceAPI/Services/Product/IProductService.cs
+++ MockClothesCommerceAPI/Services/Product/IProductService.cs
@@ -1,5 +1,5 @@
 ﻿
-using MockClothesCommerceAPI.Contracts.Product;
+using MockClothesCommerceAPI.Dtos;
 
 namespace MockClothesCommerceAPI.Services.Product;
 
@@ -9,7 +9,11 @@ public interface IProductService
     bool CreateProduct(Models.Product product);
     Models.Product GetProduct(int id);
     bool UpdateProduct(int id, UpdateProductRequest product);
+    bool AddProductToFavorites(int userId, int productId);
+    bool AddReview(Models.Review newReview);
     bool DeleteProduct(Models.Product product);
+    bool DeleteFavoriteProduct(int userId, int productId);
+    ICollection<Models.Review> GetReviews(int productId);
     bool ProductExists(int id);
     bool Save();
 }
--- MockClothesCommerceAPI/Services/Product/ProductService.cs
+++ MockClothesCommerceAPI/Services/Product/ProductService.cs
@@ -1,5 +1,6 @@
-﻿using MockClothesCommerceAPI.Contracts.Product;
-using MockClothesCommerceAPI.Data;
+﻿using MockClothesCommerceAPI.Data;
+using MockClothesCommerceAPI.Dtos;
+using MockClothesCommerceAPI.Models;
 
 namespace MockClothesCommerceAPI.Services.Product;
 
@@ -17,9 +18,9 @@ public class ProductService : IProductService
         return _context.Products.Any(u => u.Id == id);
     }
     /*Create a user*/
-    public bool CreateProduct(Models.Product Product)
+    public bool CreateProduct(Models.Product product)
     {
-        _context.Products.Add(Product);
+        _context.Products.Add(product);
         return Save();
     }
 
@@ -33,7 +34,7 @@ public class ProductService : IProductService
     /*Get a single Product*/
     public Models.Product GetProduct(int id)
     {
-        return _context.Products.Find(id);
+        return _context.Products.FirstOrDefault();
     }
 
     /*Get a list of Products*/
@@ -51,9 +52,41 @@ public class ProductService : IProductService
         if (productUpdated.Description is not null) existingProduct.Description = productUpdated.Description;
         if (productUpdated.Price != existingProduct.Price) existingProduct.Price = productUpdated.Price;
         if (productUpdated.Tags is not null) existingProduct.Tags = productUpdated.Tags;
-        // Ver que onda la lista de tags
 
         return Save();
+    }
+
+
+    public bool AddReview(Models.Review newReview)
+    {
+        _context.Reviews.Add(newReview);
+        return Save();
+    }
+
+    public ICollection<Models.Review> GetReviews(int productId)
+    {
+        var reviews = _context.Reviews
+            .Where(r => r.ProductId == productId)
+            .Select(r => new Models.Review
+            {
+                Id = r.Id,
+                Content = r.Content,
+                Rating = r.Rating,
+                CreatedAt = r.CreatedAt,
+                ModifiedAt = r.ModifiedAt,
+                User = new Models.User
+                {
+                    Id = r.User.Id,
+                    Username = r.User.Username,
+                },
+                Product = new Models.Product
+                {
+                    Id = r.Product.Id,
+                    Name = r.Product.Name,
+                }
+            });
+
+        return reviews.ToList();
     }
 
     public bool Save()
@@ -63,6 +96,33 @@ public class ProductService : IProductService
         return saved > 0;
     }
 
+    public bool AddProductToFavorites(int userId, int productId)
+    {
+        var user = _context.Users.Find(userId);
+        var product = _context.Products.Find(productId);
+
+        var favoriteProduct = new FavoriteProduct
+        {
+            UserId = userId,
+            ProductId = productId,
+            User = user,
+            Product = product
+        };
+
+        _context.Favorites.Add(favoriteProduct);
+
+        return Save();
+    }
+
+    public bool DeleteFavoriteProduct(int userId, int productId)
+    {
+        var favoriteToDelete = _context.Favorites
+            .Where(fp => fp.UserId == userId && fp.ProductId == productId)
+            .FirstOrDefault();
+        _context.Favorites.Remove(favoriteToDelete);
+
+        return Save();
+    }
 }
 
 
