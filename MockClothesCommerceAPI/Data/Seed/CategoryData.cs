69a19b09066eeb87acc3806ef0d20b3f261612e1
srsebbi <csperichon@gmail.com>
2023-04-28 23:20:20 -0300

    Added seed category data

---
+++ MockClothesCommerceAPI/Data/Seed/CategoryData.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MockClothesCommerceAPI.Data;
+using MockClothesCommerceAPI.Models;
+
+namespace MockClothesCommerceAPI.Data.Seed;
+public static class CategorySeed
+{
+  public static void Initialize(WebApplication app)
+  {
+    var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetService<DataContext>();
+
+    context.Database.EnsureCreated();
+    if (!context.Users.Any())
+    {
+      context.Categories.Add(new Category
+      {
+        Id = 1,
+        Name = "Hats",
+        CreatedAt = DateTime.UtcNow,
+        ModifiedAt = DateTime.UtcNow
+      });
+
+      context.Categories.Add(new Category
+      {
+        Id = 2,
+        Name = "Jewelry",
+        CreatedAt = DateTime.UtcNow,
+        ModifiedAt = DateTime.UtcNow
+      });
+
+
+      context.Categories.Add(new Category
+      {
+        Id = 3,
+        Name = "Women",
+        CreatedAt = DateTime.UtcNow,
+        ModifiedAt = DateTime.UtcNow
+      });
+
+
+      context.Categories.Add(new Category
+      {
+        Id = 4,
+        Name = "Men",
+        CreatedAt = DateTime.UtcNow,
+        ModifiedAt = DateTime.UtcNow
+      });
+
+      context.SaveChanges();
+    }
+  }
+}
