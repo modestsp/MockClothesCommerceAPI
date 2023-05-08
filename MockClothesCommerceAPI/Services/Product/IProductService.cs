1700e5218b0c0b44734fa4c7222e8bf2d9255042
srsebbi <csperichon@gmail.com>
2023-05-08 19:53:51 -0300

    Updated return type

--- MockClothesCommerceAPI/Services/Product/IProductService.cs
+++ MockClothesCommerceAPI/Services/Product/IProductService.cs
@@ -5,16 +5,16 @@ namespace MockClothesCommerceAPI.Services.Product;
 
 public interface IProductService
 {
-  Task<ICollection<Models.Product>> GetProducts();
-  Task<bool> CreateProduct(Models.Product product);
-  Task<Models.Product> GetProduct(int id);
-  Task<bool> UpdateProduct(int id, UpdateProductRequest product);
-  Task<bool> AddProductToFavorites(int userId, int productId);
-  Task<bool> AddReview(Models.Review newReview);
-  Task<bool> DeleteProduct(Models.Product product);
-  Task<bool> DeleteFavoriteProduct(int userId, int productId);
-  Task<ICollection<Models.Review>> GetReviews(int productId);
-  Task<bool> ProductExists(int id);
-  Task<bool> Save();
+    Task<ICollection<Models.Product>> GetProducts();
+    Task<bool> CreateProduct(Models.Product product);
+    Task<Models.Product> GetProduct(int id);
+    Task<Models.Product> UpdateProduct(int id, UpdateProductRequest product);
+    Task<bool> AddProductToFavorites(int userId, int productId);
+    Task<bool> AddReview(Models.Review newReview);
+    Task<bool> DeleteProduct(Models.Product product);
+    Task<bool> DeleteFavoriteProduct(int userId, int productId);
+    Task<ICollection<Models.Review>> GetReviews(int productId);
+    Task<bool> ProductExists(int id);
+    Task<bool> Save();
 }
 
