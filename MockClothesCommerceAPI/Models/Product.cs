31a69936798fa89c10ee004bbc96bafe8c8e3037
srsebbi <csperichon@gmail.com>
2023-05-08 19:54:24 -0300

    Added GetStars method

--- MockClothesCommerceAPI/Models/Product.cs
+++ MockClothesCommerceAPI/Models/Product.cs
@@ -5,14 +5,23 @@ public class Product
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public string Url { get; set; }
+    public string? Url { get; set; }
     public decimal Price { get; set; } = 0;
     public decimal Stars { get; set; } = 0;
-    public string PhotographerUrl { get; set; }
-    public string PhotographerName { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime ModifiedAt { get; set; }
+    public string? PhotographerUrl { get; set; }
+    public string? PhotographerName { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime ModifiedAt { get; set; } = DateTime.Now;
     public int CategoryId { get; set; }
-    public Category Category { get; set; }
+    public List<string>? Tags { get; set; }
+
+    public static decimal GetStars(List<Review> reviews)
+    {
+        if (reviews == null || reviews.Count == 0)
+        {
+            return 0;
+        }
+        return reviews.Sum(r => r.Rating) / reviews.Count;
+    }
 }
 
