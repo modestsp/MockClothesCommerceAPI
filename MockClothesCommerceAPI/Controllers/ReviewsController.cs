0529b1a977c49095d72e3aee5e22600ad904c7b5
srsebbi <csperichon@gmail.com>
2023-05-08 19:56:15 -0300

    Updated route and added async code

--- MockClothesCommerceAPI/Controllers/ReviewsController.cs
+++ MockClothesCommerceAPI/Controllers/ReviewsController.cs
@@ -4,102 +4,101 @@ using MockClothesCommerceAPI.Services.Review;
 
 namespace MockClothesCommerceAPI.Controllers;
 
-[Route("api/[controller]")]
+[Route("[controller]")]
 [ApiController]
 public class ReviewsController : ControllerBase
 {
-  private readonly IReviewService _reviewService;
+    private readonly IReviewService _reviewService;
 
-  public ReviewsController(IReviewService reviewService)
-  {
-    _reviewService = reviewService;
-  }
-
-  [HttpGet]
-  public async Task<IActionResult> GetReviews()
-  {
-    var reviews = _reviewService.GetReviews();
-
-    return Ok(reviews);
-  }
-
-  [HttpGet("{reviewId}")]
-  [ProducesResponseType(404)]
-  [ProducesResponseType(200)]
-  public async Task<IActionResult> GetReview(int reviewId)
-  {
-    if (!await _reviewService.ReviewExists(reviewId)) return NotFound();
-
-    var review = await _reviewService.GetReview(reviewId);
-
-    if (!ModelState.IsValid) return BadRequest(ModelState);
-
-    return Ok(review);
-  }
-
-  // [HttpPost]
-  //  public async Task<IActionResult> CreateReview([FromBody] CreateReviewRequest request)
-  //  {
-  //     if (request is null) return BadRequest(ModelState);
-
-  //     if (!ModelState.IsValid) return BadRequest(ModelState);
-
-  //    var newReview = new Review
-  //     {
-  //         Content = request.Content,
-  //         Rating = request.Rating,
-  //        UserId = request.UserId,
-  //  ProductId = request.ProductId,
-  //        CreatedAt = DateTime.UtcNow,
-  //        ModifiedAt = DateTime.UtcNow
-  //    };
-
-  //    if (!await _reviewService.CreateReview(newReview))
-  //    {
-  //       ModelState.AddModelError("", "Something went wrong while savin");
-  //        return StatusCode(500, ModelState);
-  //   }
-
-  //    return Ok(newReview);
-  //   }
-
-  [HttpPut("{reviewId}")]
-  public async Task<IActionResult> UpdateReview([FromRoute] int reviewId, UpdateReviewRequest updateReviewRequest)
-  {
-
-    if (updateReviewRequest == null)
-      return BadRequest(ModelState);
-
-    if (!await _reviewService.ReviewExists(reviewId))
-      return NotFound();
-
-    var existingReview = await _reviewService.GetReview(reviewId);
-
-    if (existingReview.UserId != updateReviewRequest.UserId) return Unauthorized();
-    if (existingReview.ProductId != updateReviewRequest.ProductId) return BadRequest();
-    if (!ModelState.IsValid)
-      return BadRequest(ModelState);
-
-
-    if (!await _reviewService.UpdateReview(reviewId, updateReviewRequest))
+    public ReviewsController(IReviewService reviewService)
     {
-      ModelState.AddModelError("", "Something went wrong updating review");
-      return StatusCode(500, ModelState);
+        _reviewService = reviewService;
     }
 
-    return NoContent();
-  }
+    [HttpGet]
+    public async Task<IActionResult> GetReviews()
+    {
+        var reviews = await _reviewService.GetReviews();
+
+        return Ok(reviews);
+    }
+
+    [HttpGet("{reviewId}")]
+    [ProducesResponseType(404)]
+    [ProducesResponseType(200)]
+    public async Task<IActionResult> GetReview(int reviewId)
+    {
+        if (!await _reviewService.ReviewExists(reviewId)) return NotFound();
+
+        var review = await _reviewService.GetReview(reviewId);
+
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        return Ok(review);
+    }
+
+    // [HttpPost]
+    //  public async Task<IActionResult> CreateReview([FromBody] CreateReviewRequest request)
+    //  {
+    //     if (request is null) return BadRequest(ModelState);
+
+    //     if (!ModelState.IsValid) return BadRequest(ModelState);
+
+    //    var newReview = new Review
+    //     {
+    //         Content = request.Content,
+    //         Rating = request.Rating,
+    //        UserId = request.UserId,
+    //  ProductId = request.ProductId,
+    //        CreatedAt = DateTime.UtcNow,
+    //        ModifiedAt = DateTime.UtcNow
+    //    };
+
+    //    if (!await _reviewService.CreateReview(newReview))
+    //    {
+    //       ModelState.AddModelError("", "Something went wrong while savin");
+    //        return StatusCode(500, ModelState);
+    //   }
+
+    //    return Ok(newReview);
+    //   }
+
+    [HttpPut("{reviewId}")]
+    public async Task<IActionResult> UpdateReview([FromRoute] int reviewId, UpdateReviewRequest updateReviewRequest)
+    {
+
+        if (updateReviewRequest is null)
+            return BadRequest(ModelState);
+
+        if (!await _reviewService.ReviewExists(reviewId))
+            return NotFound();
+
+        var existingReview = await _reviewService.GetReview(reviewId);
+        if (existingReview.UserId != updateReviewRequest.UserId) return Unauthorized();
+        if (existingReview.ProductId != updateReviewRequest.ProductId) return BadRequest();
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+
+        if (!await _reviewService.UpdateReview(reviewId, updateReviewRequest))
+        {
+            ModelState.AddModelError("", "Something went wrong updating review");
+            return StatusCode(500, ModelState);
+        }
+
+        return NoContent();
+    }
 
 
 
-  [HttpDelete("{id}")]
-  public async Task<IActionResult> DeleteReview(int id)
-  {
-    var reviewToDelete = await _reviewService.GetReview(id);
-    if (reviewToDelete is null) return NotFound();
-    await _reviewService.DeleteReview(reviewToDelete);
-    return NoContent();
-  }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteReview(int id)
+    {
+        var reviewToDelete = await _reviewService.GetReview(id);
+        if (reviewToDelete is null) return NotFound();
+        await _reviewService.DeleteReview(reviewToDelete);
+        return NoContent();
+    }
 
 }
 
