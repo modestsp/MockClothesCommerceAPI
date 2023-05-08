c8a38b55a43c98c641f82ad700f3d3c39368d820
srsebbi <csperichon@gmail.com>
2023-05-08 19:54:45 -0300

    Added Created and modified props

--- MockClothesCommerceAPI/Models/Review.cs
+++ MockClothesCommerceAPI/Models/Review.cs
@@ -4,9 +4,12 @@ public class Review
 {
     public int Id { get; set; }
     public string Content { get; set; }
+    public decimal Rating { get; set; }
     public int UserId { get; set; }
     public User User { get; set; }
     public int ProductId { get; set; }
     public Product Product { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime ModifiedAt { get; set; } = DateTime.Now;
 }
 
