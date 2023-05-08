b28d3ddd615951163aa287040fbd0ffa532e1318
srsebbi <csperichon@gmail.com>
2023-05-08 19:56:50 -0300

    Removed comment

--- MockClothesCommerceAPI/Models/Category.cs
+++ MockClothesCommerceAPI/Models/Category.cs
@@ -2,19 +2,10 @@
 
 public class Category
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime ModifiedAt { get; set; }
-    public ICollection<Product> Products { get; set; }
-
-    //  public Category(
-    //     int id,
-    //    string name,
-    //   DateTime createdAt,
-    //  DateTime modifiedAt)
-    // {
-    //
-    //   }
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime ModifiedAt { get; set; }
+  // public ICollection<Product> Products { get; set; }
 }
 
