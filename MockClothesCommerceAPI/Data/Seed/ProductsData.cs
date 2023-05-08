4adecef4bdad7a3e10b9c54e9ed5b1db80d9a5e5
srsebbi <csperichon@gmail.com>
2023-05-08 20:07:52 -0300

    Fixed Date

--- MockClothesCommerceAPI/Data/Seed/ProductsData.cs
+++ MockClothesCommerceAPI/Data/Seed/ProductsData.cs
@@ -1,6 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using MockClothesCommerceAPI.Data;
 using MockClothesCommerceAPI.Models;
 
 namespace MockClothesCommerceAPI.Data.Seed;
@@ -21,8 +18,8 @@ public static class ProductSeed
                 Url = "https://images.unsplash.com/photo-1620231109648-302d034cb29b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
                 Description = "Love Disorder blue is a beautiful blue hat that you can wear ",
                 Stars = 0,
-                CreatedAt = DateTime.Parse("16/1/2023 8:41:28"),
-                ModifiedAt = DateTime.Parse("16/1/2023 8:41:28"),
+                CreatedAt = DateTime.Parse("2023-01-16 8:41:28"),
+                ModifiedAt = DateTime.Parse("2023-01-16 8:41:28"),
                 CategoryId = 1,
                 PhotographerName = "Andre Styles",
                 PhotographerUrl = "https://unsplash.com/es/@lovedisorder",
