7f16c647fd8656e66523f410766f864665710e28
srsebbi <csperichon@gmail.com>
2023-05-08 19:54:11 -0300

    Added constraints for min and max name length

--- MockClothesCommerceAPI/Models/User.cs
+++ MockClothesCommerceAPI/Models/User.cs
@@ -1,15 +1,19 @@
-﻿namespace MockClothesCommerceAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+namespace MockClothesCommerceAPI.Models;
 
 public class User
 {
+    public const int MinUsernameLength = 3;
+    public const int MaxUsernameLength = 20;
+
     public int Id { get; set; }
     public string Username { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
+    // Check for confirmPassword
     public string Password { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime ModifiedAt { get; set; } = DateTime.Now;
-    public ICollection<Review> Reviews { get; set; }
 
 }
 
