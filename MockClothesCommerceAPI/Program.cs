801f72910ceef644eba81bae492586fe7c8dfecf
srsebbi <csperichon@gmail.com>
2023-05-08 19:55:24 -0300

    Added cors and health check

--- MockClothesCommerceAPI/Program.cs
+++ MockClothesCommerceAPI/Program.cs
@@ -1,3 +1,4 @@
+using System.Buffers;
 using Microsoft.EntityFrameworkCore;
 using MockClothesCommerceAPI.Data;
 using MockClothesCommerceAPI.Data.Seed;
@@ -9,19 +10,31 @@ using MockClothesCommerceAPI.Services.User;
 
 var builder = WebApplication.CreateBuilder(args);
 {
-  builder.Services.AddControllers();
-  builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-  builder.Services.AddEndpointsApiExplorer();
-  builder.Services.AddSwaggerGen();
-  builder.Services.AddScoped<IUserService, UserService>();
-  builder.Services.AddScoped<IProductService, ProductService>();
-  builder.Services.AddScoped<IReviewService, ReviewService>();
-  builder.Services.AddScoped<ICategoryService, CategoryService>();
-  builder.Services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("MockClothesCommerce"));
+    builder.Services.AddControllers();
+    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddScoped<IUserService, UserService>();
+    builder.Services.AddScoped<IProductService, ProductService>();
+    builder.Services.AddScoped<IReviewService, ReviewService>();
+    builder.Services.AddScoped<ICategoryService, CategoryService>();
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowAnyOrigin", builder =>
+      {
+          builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+      });
+    });
+    builder.Services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("MockClothesCommerce"));
+    builder.Services.AddHealthChecks();
 }
 
 
 var app = builder.Build();
+app.MapHealthChecks("/health");
 CategorySeed.Initialize(app);
 ProductSeed.Initialize(app);
 UserSeed.Initialize(app);
@@ -30,12 +43,14 @@ ReviewSeed.Initialize(app);
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.UseSwagger();
-  app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 //app.UseExceptionHandler("/error");
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAnyOrigin");
 
 app.UseAuthorization();
 
